0e1ac0fcff4d7ede5b7cf07ae81fb0e66eed12c0
MickeyWeiss <167358315+MickeyWeiss@users.noreply.github.com>
2024-06-15 11:13:57 -0600

    Add files via upload
    Updated/fixed and finished assignment.

--- Block15/froyo.js
+++ Block15/froyo.js
@@ -1,11 +1,17 @@
-const userInputString = prompt(
-    "Please enter some Frozen Yogurt flavors separated by commas.", "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
-  );
-  const stringArray = userInputString.split(",");
-  console.log(stringArray);
 
-let order = {}
-for (let i = 0; i < stringArray.length; i++ ){
-  let order = stringArray[i]
-  console.table(order)
-}
+let flavors = prompt('Please enter frozen yogurt flavors seperated by commas.', 'Vanilla,Vanilla,Vanilla,Strawberry,Coffee,Coffee');
+
+flavors = flavors.split(',')
+
+console.log('An array of flavors:', flavors)
+
+let froyo = {}
+  for (let i = 0; i < flavors.length; i++) {
+    let flavor = flavors[i].trim()
+    if (!froyo[flavor]){
+      froyo[flavor] = 1
+    }else {
+      froyo[flavor] += 1
+    }
+  }
+console.table(froyo)
